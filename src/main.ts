6bdd7b2ffb255aa0e45d26568e92efc82ccc55fe
Diana <dianagomez151019@gmail.com>
2025-03-18 13:09:16 -0500

    update origin cors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS
   app.enableCors({
-    origin: 'http://localhost:3000', // Permite peticiones desde el frontend
+    origin: [
+      'http://localhost:3000', 
+      'https://trueque360-gkkq8c5cc-dianag2022s-projects.vercel.app/' // Reemplaza con tu URL real
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
+  
 
   await app.listen(3001);
 }
